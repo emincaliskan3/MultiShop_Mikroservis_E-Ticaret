e58ef37b0fce6e6988d1c8169ea1cf62fd20e5b1
emincaliskan3 <emincaliskan788@hotmail.com>
2024-11-21 18:47:53 +0300

    UI

---
+++ Frontends/MultiShop.WebUI/Controllers/ContactController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.Controllers
+{
+    public class ContactController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.directory1 = "MultiShop";
+            ViewBag.directory2 = "İletişim";
+            ViewBag.directory3 = "Mesaj Gönder";
+            return View();
+        }
+    }
+}
