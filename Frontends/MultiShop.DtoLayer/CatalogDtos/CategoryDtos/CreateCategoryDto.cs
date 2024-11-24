caa121182785a1f3602ec291926722d7311ffb05
emincaliskan3 <emincaliskan788@hotmail.com>
2024-11-24 23:47:39 +0300

    CategoryDtıs

---
+++ Frontends/MultiShop.DtoLayer/CatalogDtos/CategoryDtos/CreateCategoryDto.cs
@@ -0,0 +1,10 @@
+﻿
+
+namespace MultiShop.DtoLayer.CatalogDtos.CategoryDtos
+{
+    public class CreateCategoryDto
+    {
+        public string CategoryName { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
---
+++ Frontends/MultiShop.DtoLayer/CatalogDtos/CategoryDtos/UpdateCategoryDto.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace MultiShop.DtoLayer.CatalogDtos.CategoryDtos
+{
+    public class UpdateCategoryDto
+    {
+        public string CategoryID { get; set; }
+        public string CategoryName { get; set; }
+        public string ImageUrl { get; set; }
+
+    }
+}
